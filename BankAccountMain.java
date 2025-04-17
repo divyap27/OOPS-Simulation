44ba1e56eaedcef45ed8073c01476a12a0a2fb76
divyap27 <pawardivya221@gmail.com>
2025-04-17 16:13:31 +0530

    Initial Commit

--- BankAccountMain.java
+++
@@ -1,8 +0,0 @@
-class BankAccountMain{
-    public static void main(String[] args) {
-        BankAccount bankaccount =new BankAccount();
-        bankaccount.deposit(500);
-        bankaccount.withdraw(300);
-        bankaccount.checkbalance();
-    }
-}
--- NamePrinting.java
+++
@@ -1,7 +0,0 @@
-class NamePrinting {
- 	public static void main(String[] args) {
- 	System.out.println("Divya");
-        System.out.println("Divya");
-
-	}
-}
