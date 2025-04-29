92ce7ec2b493bf5c7a8a056ced5d4a97208fdd0e
divyap27 <pawardivya221@gmail.com>
2025-04-29 11:47:06 +0530

    Kept only MusicAppSimulation.java in GitHub

--- ReverseArray.java
+++
@@ -1,9 +0,0 @@
-public class ReverseArray {
-    public static void main(String[] args) {
-        int[] arr = {10, 20, 30, 40, 50};
-        System.out.println("Reversed array:");
-        for (int i = arr.length - 1; i >= 0; i--) {
-            System.out.print(arr[i] + " ");
-        }
-    }
-}
